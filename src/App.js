4feaa73ed9b08815dc029bb20f7b1144b0e92433
David Jr <55713829+Daviiro@users.noreply.github.com>
2020-07-14 23:46:49 -0300

    altearacoes para chamar o header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+
 
 import GlobalStyle from './styles';
 import Principal from './views/Principal';
+import Header from  './views/header/header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <GlobalStyle />
       <Principal />
+
     </BrowserRouter>
   );
+
 }
 
 export default App;
+

89eddd728fe066a694b35184c099792484173b1b
Nicolas Matias <nicolasmarcelo30@gmail.com>
2020-07-23 17:18:51 -0300

    mudei o cardapio

--- src/views/cardapio/style/cardapio.js
+++ src/views/cardapio/style/cardapio.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Content = styled.div`
 .ant-carousel .slick-slide {
   text-align: center;
-  background: rgb(255, 61, 61);
+  background: white;
   overflow: hidden;
 }
 

99b091cd711aa7277d97933d3ca1143c17ca5e81
Nicolas Matias <nicolasmarcelo30@gmail.com>
2020-07-22 22:40:48 -0300

    contatos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './views/header/headerStyle.css';
+import './views/contato/contatostyle.css';
+import './views/cardapio/cardapiostyle.css';
 import App from './App';
 
 

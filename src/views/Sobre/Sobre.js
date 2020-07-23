e236242973ca7553ad57c87636dcfdd53cb33523
Guilherme de Aguiar <gpacianotto@gmail.com>
2020-07-23 16:56:42 -0300

    footer mais bonitinho

--- src/views/Sobre/Sobre.js
+++ src/views/Sobre/Sobre.js
@@ -10,20 +10,20 @@ return (
 
 <Content>
     
-    <div class = "titulotrsite">
+    <div className = "titulotrsite">
     
     <h1> Pizzaria da EJcomp </h1>
     
     </div>
 
-    <div class = "texto">
+    <div className = "texto">
 
      <p>Desde 2010 servindo as melhores pizzas de presidente prudente, começamos com uma simples pizzaria e poucos 
      funcionarios, mal tinhamos um caixa, não podiamos fazer entregas, nossa infraestrutura não era das melhores mas com a dedicação, talento, e perseverança hoje estamos com uma ampla infraestrutura para atender a todos os pedidos da região, agora com o nosso novo site todos poderão saber sobre a melhor pizzaria da região.</p>
 
     </div>
 
-    <div class = "titulotrsite">
+    <div className = "titulotrsite">
 
      <h1> A nossa cozinha </h1>
 
@@ -31,7 +31,7 @@ return (
 
     <Carousel autoplay>
 
-    <div class="imagem">
+    <div className="imagem">
 
         <img src="https://media-cdn.tripadvisor.com/media/photo-s/0b/00/e7/0a/sabor-vaipira-cozinha.jpg" width="660" height="350"></img>
 
@@ -45,7 +45,7 @@ return (
         
     </Carousel>
 
-    <div class = "texto">
+    <div className = "texto">
 
       <p>Sempre haviam dificuldades com a cozinha, especialmente com o forno, muitas vezes nós não consiguiamos dar conta dos pedidos pelo fato de termos apenas 1 forno para pizzas além do espaço minusculo para fazer o preparo delas, ingredientes eram limitados e também não tinhamos uma grande variedade de cardápio devivdo aos limites que os equipamentos disponiveis nos davam, mas hoje tudo mudou, temos tudo oque precisamos e não vamos parar por ai, vamos espandir sempre para trazer a maior variedade de pizzas para todos os nossos clientes.</p>
 
@@ -53,7 +53,7 @@ return (
 
     <hr></hr>
 
-    <div class = "titulotrsite">
+    <div className = "titulotrsite">
 
      <h1> Nossos funcionarios </h1>
 
@@ -61,7 +61,7 @@ return (
 
     <Carousel autoplay>
 
-    <div class="imagem">
+    <div className="imagem">
 
         <img src="https://informa.life/wp-content/uploads/2020/04/Revista-Aquarius-Life-116-pizza-Xama.jpg
 " width="660" height="350"></img>
@@ -77,7 +77,7 @@ return (
         
     </Carousel>
 
-    <div class = "texto">
+    <div className = "texto">
 
      <p>Profissionais qualificados no ramo de gastronomia, competentes e habéis no serviço, sem eles a nossa pizzaria não é nada, eles são responsavéis pelo sucesso que tivemos e teremos futuramente, eles estão prontos para anotar, realizar e entregar o seu pedido em qualquer lugar da região.</p>
 
--- src/views/footer/Footer.js
+++ src/views/footer/Footer.js
@@ -16,17 +16,62 @@ export default class Footer extends Component {
                     <p className="textoTitulo">
                     Menu
                     </p>
+                    <p className = "textoPadrao">
+                        -Home 
+                    </p>
+                    <p className = "textoPadrao">
+                        -Cardápio
+                    </p>
+                    <p className = "textoPadrao">
+                        -Sobre Nós 
+                    </p>
+                    <p className = "textoPadrao">
+                        -Contato
+                    </p>
+
+                        
 
                 </div>
                 <div className="gomo">
                     <p className="textoTitulo">
                     Localização
                     </p>
+                    <p className = "textoPadrao">
+                        Rua dos Alfeneiros, N° 40
+                    </p>
+                    <p className = "textoPadrao">
+                        Bairro Humberto Salvador
+                    </p>
+                    <p className = "textoPadrao">
+                        Presidente Prudente, SP
+                    </p>
+                    <p className = "textoPadrao">
+                        umemailqualquer@gmail.com
+                    </p>
 
                 </div>
                 <div className="gomo">
                     <p className="textoTitulo">
                     Contato
+                    </p>
+                    <img className="iconeContato" src={require('./email icon.png')} />
+                    <p className = "textoPadrao">
+                        umemailqualquer@gmail.com
+                    </p>
+                    <img className="iconeContato" src={require('./zap.png')}id='zap' />
+                    <p className = "textoPadrao">
+                        (18) 99045 3898
+                    </p>
+                    <img className="iconeContato" src={require('./zap.png')}id='zap' />
+                    <p className = "textoPadrao">
+                        (18) 3900 5578
+                    </p>
+                    <img className="iconeContato" src={require('./mapa.png')}id='mapa' />
+                    <p className = "textoPadrao">
+                        Veja no mapa
+                    </p>
+                    <p className = "textoPadrao">
+                        
                     </p>
 
                 </div>
